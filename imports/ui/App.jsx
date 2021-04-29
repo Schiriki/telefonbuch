a54a2271304f04d85ca76134473cb2c2b2a61229
Schiriki <78704598+Schiriki@users.noreply.github.com>
2021-04-29 15:43:53 +0200

    Collection base

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import { Contact } from './Contact.jsx';
+import { useTracker } from 'meteor/react-meteor-data';
+import { ContactCollection } from '/imports/api/ContactCollection';
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
-  </div>
-);
+export const App = () => {
+	const contacts = useTracker(() => ContactCollection.find({}).fetch());
+
+	return (
+		<div>
+			<h1>Welcome to Meteor!</h1>
+
+			<ul>
+				{ contacts.map(contact => 
+					<Contact 
+						key = { contact._id } 
+						contact = { contact }
+					/>) }
+			</ul>
+		</div>
+	)
+};
--- server/main.js
+++ server/main.js
@@ -1,31 +1,18 @@
 import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/api/links';
+import { ContactCollection } from '/imports/api/ContactCollection';
 
-function insertLink({ title, url }) {
-  LinksCollection.insert({title, url, createdAt: new Date()});
-}
-
+const insertContact = contactName => ContactCollection.insert({ name: contactName });
+ 
 Meteor.startup(() => {
-  // If the Links collection is empty, add some data.
-  if (LinksCollection.find().count() === 0) {
-    insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://www.meteor.com/tutorials/react/creating-an-app'
-    });
-
-    insertLink({
-      title: 'Follow the Guide',
-      url: 'http://guide.meteor.com'
-    });
-
-    insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com'
-    });
-
-    insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com'
-    });
+  if (ContactCollection.find().count() === 0) {
+    [
+      'Luffy',
+      'Kaneki',
+      'Asuna',
+      'Eren',
+      'Kirito',
+      'Law',
+      'Ace'
+    ].forEach(insertContact)
   }
 });
